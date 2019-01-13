6e349e8d9312d5cd0aea7897f2fb31c15640cb39
matthewmcnew <me@mattmcnew.com>
2019-01-13 01:17:21 -0600

    Add Raspberry Pi Support

--- zsh/tv.zsh
+++ zsh/tv.zsh
@@ -1,3 +1,11 @@
-alias tvon="ssh pi@raspberrypi 'zsh -t -ci \"tvon\"'"
+if [ "${hostname}" != "raspberrypi" ]
+then
+    alias tvon="ssh pi@raspberrypi 'zsh -t -ci \"tvon\"'"
+    alias tvoff="ssh pi@raspberrypi 'zsh -t -ci \"tvoff\"'"
+else
+    alias tvon="echo on 0 | cec-client -s -d 1 && echo \"tx 4F:82:10:00\" | cec-client -s -d 1"
+    alias tvpi="echo \"tx 4F:82:10:00\" | cec-client -s -d 1"
+    alias tvoff="echo standby 0 | cec-client -s -d 1"
+    alias tvchromecast="echo \"tx 4F:82:20:00\" | cec-client -s -d 1"
+fi
 
-alias tvoff="ssh pi@raspberrypi 'zsh -t -ci \"tvoff\"'"
