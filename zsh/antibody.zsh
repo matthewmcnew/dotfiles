b642515b866c7f5236f949f907e3343b834fb8e5
matthewmcnew <me@mattmcnew.com>
2019-01-27 21:19:07 -0600

    add upgrade_all functionality

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -1,4 +1,4 @@
-update_zsh(){
+upgrade_antibody(){
   antibody bundle <~/workspace/dotfiles/plugins.txt >~/.zsh_plugins.sh
   antibody update
   source ~/.zshrc
--- zsh/upgrade.zsh
+++ zsh/upgrade.zsh
@@ -1,13 +1,25 @@
-function upgrade_all() {
-
-    set -x
-    echo "upgrading"
-
+function upgrade_brew(){
+    echo "upgrading brew"
     brew update
 
     brew outdated
     brew cask outdated
 
     brew upgrade
+    brew cask upgrade
+}
 
+function upgrade_all() {
+    set -x
+    upgrade_dotfiles
+    upgrade_brew
+    set +x
+    upgrade_antibody
+    softwareupdate -i -a
+}
+
+function upgrade_dotfiles(){
+    pushd ~/workspace/dotfiles
+        git pull
+    popd
 }
